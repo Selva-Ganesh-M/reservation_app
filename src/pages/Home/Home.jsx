f9cd1e1f0ff0b851ab6f89aa5775bf2f1f5f8668
Selva Ganesh M <selvadev2k@gmail.com>
2023-06-22 20:38:54 +0530

    properties list UI

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,6 +1,7 @@
 import Header from "../../components/Header/Header"
 import Navbar from "../../components/Navbar/Navbar"
 import Featured from "../../components/featured/Featured"
+import PropertyList from "../../components/propertyList/PropertyList"
 import "./home.css"
 
 const Home = () => {
@@ -10,6 +11,8 @@ const Home = () => {
             <Header />
             <div className="homeContainer">
                 <Featured />
+                <h1 className="homeTitle">Browse by property type</h1>
+                <PropertyList />
             </div>
         </div>
     )
