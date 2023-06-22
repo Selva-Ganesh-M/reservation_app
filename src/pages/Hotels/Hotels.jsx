ad7c8e199d8224ca2a78ff53fde5c22492fdbc33
Selva Ganesh M <selvadev2k@gmail.com>
2023-06-23 00:21:31 +0530

    hotels page layout

--- src/pages/Hotels/Hotels.jsx
+++ src/pages/Hotels/Hotels.jsx
@@ -7,6 +7,22 @@ const Hotels = () => {
         <div>
             <Navbar />
             <Header type="list" />
+            <div className="container">
+                <div className="wrapper">
+                    <div className="search">
+                        <h1 className="searchTitle">Search</h1>
+                        <div className="searchItem">
+                            <label htmlFor="">Destination</label>
+                            <input type="text" />
+                        </div>
+                        <div className="searchItem">
+                            <label htmlFor="">Check-in Date</label>
+
+                        </div>
+                    </div>
+                    <div className="results"></div>
+                </div>
+            </div>
         </div>
     )
 }
