a4c1acf29ef20965aa28d2b1864707751f89777d
Selva Ganesh M <selvadev2k@gmail.com>
2023-06-22 22:35:19 +0530

    featured properties UI

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,6 +1,7 @@
 import Header from "../../components/Header/Header"
 import Navbar from "../../components/Navbar/Navbar"
 import Featured from "../../components/featured/Featured"
+import FeaturedProperties from "../../components/featuredProperties/FeaturedProperties"
 import PropertyList from "../../components/propertyList/PropertyList"
 import "./home.css"
 
@@ -13,6 +14,8 @@ const Home = () => {
                 <Featured />
                 <h1 className="homeTitle">Browse by property type</h1>
                 <PropertyList />
+                <h1 className="homeTitle">Homes guests love</h1>
+                <FeaturedProperties />
             </div>
         </div>
     )
